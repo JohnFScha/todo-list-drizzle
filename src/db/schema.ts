07eba2280cc7e3b72a8bdb7d8e064368c6c41c37
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2024-06-21 18:48:41 -0300

    connected to db

---
+++ src/db/schema.ts
@@ -0,0 +1,27 @@
+import { pgTable, serial, text, varchar, boolean, date } from 'drizzle-orm/pg-core';
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Client } from "pg";
+const credentials = process.env.NEXT_PUBLIC_PG_CONNECTION_STRING;
+
+const client = new Client({
+  connectionString: credentials,
+});
+
+client.connect()
+  .then(() => console.log('Connected to DB'))
+  .catch((error) => console.log(`Error connecting to DB: ${error.message}`));
+
+const db = drizzle(client);
+
+export const todos = pgTable('todo', {
+  id: serial('id').primaryKey(),
+  title: text('title'),
+  description: varchar('description', { length: 256 }),
+  done: boolean('done').default(false),
+  createdAt: date('createdAt').default(new Date().toISOString()),
+  updatedAt: date('updatedAt').default(new Date().toISOString())
+});
+
+export type Todo = typeof todos.$inferSelect; // return type when queried
+export type NewTodo = typeof todos.$inferInsert; // insert type
+export default db;
